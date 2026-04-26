5d5ab387e93fec7dfd69c53b604ed7baf04f1ec2
JMiahman1 <antigravity@example.com>
2026-04-25 21:10:51 -0700

    Don't run the Mac Hardware tests that require UI interaction

--- tests/hardware/test_ui_mac.py
+++ tests/hardware/test_ui_mac.py
@@ -1,12 +1,13 @@
 import sys
-
+import os
 import pytest
 from PySide6.QtTest import QTest
 from PySide6.QtWidgets import QApplication
 
-# Only run these tests on macOS with a GUI session
+# Only run these tests on macOS with a physical GUI session
 pytestmark = pytest.mark.skipif(
-    sys.platform != "darwin", reason="Hardware UI tests require a physical macOS graphical session."
+    sys.platform != "darwin" or os.environ.get("CI") == "true", 
+    reason="Hardware UI tests require a physical macOS graphical session."
 )
 
 try:
